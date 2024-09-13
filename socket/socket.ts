f7374e305b8051b48f7aec14593ed44630597077
usman09s <147527912+usman09s@users.noreply.github.com>
2024-09-13 16:34:39 +0500

    Update socket.ts

--- socket/socket.ts
+++ socket/socket.ts
@@ -1,7 +1,7 @@
 import io, { Socket } from "socket.io-client";
 import { toast } from "sonner";
 
-const BASE_SOCKET_CONNECTION = "https://backend.storytime.social";
+const BASE_SOCKET_CONNECTION = "http://storytime.yameenyousuf.com";
 // const BASE_SOCKET_CONNECTION = "http://localhost:3021";
 class WSService {
   private socket: Socket | null = null;
